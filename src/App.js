19b45734464373357923ee56209487d9bc9f8fbb
khanhhy26102002 <ledangkhanhhy@gmail.com>
2025-01-24 13:47:45 +0700

    finish file introduce

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Introduce from './Components/Introduction/Introduce';
-
-
 function App() {
   return (
     <Routes>
