fa4403a804723432a4a93e8f7b695a5ae089d2b5
khanhhy26102002 <ledangkhanhhy@gmail.com>
2025-01-25 16:54:47 +0700

    update file introduce

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 import styles from "./Footer.module.scss";
 export default function Footer() {
   return (
     <div className={styles.sitefooter}>
-    <footer className={styles.siteinfo}>
-      <div className={`${styles.insidesiteinfo} ${styles.gridcontainer}`}>
-        <div className={styles.copyrightbar}>
-          <span className={styles.copyright}>
-            © 2025 Trung Tâm Gia Sư Biên Hòa
-          </span>{" "}
-          • Tạo ra với <a href="/">GeneratePress</a>
+      <footer className={styles.siteinfo}>
+        <div className={`${styles.insidesiteinfo} ${styles.gridcontainer}`}>
+          <div className={styles.copyrightbar}>
+            <span className={styles.copyright}>
+              Trung tâm Gia sư Giỏi Biên Hòa – Nơi kiến thức thăng hoa, thành
+              công bắt đầu!
+            </span>{" "}
+          </div>
         </div>
-      </div>
-    </footer>
-  </div>
-  )
+      </footer>
+    </div>
+  );
 }
