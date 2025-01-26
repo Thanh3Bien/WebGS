20c316e7ee0abd3d823231a6115c81f2e873323c
Thanh3Bien <thanhlv1793@gmail.com>
2025-01-26 23:44:44 +0700

    add function contact support

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Home from './Components/Home/Home';
 import Introduce from './Components/Introduction/Introduce';
 import Register from './Components/RegisterCourse/Register';
 import Privacy from './Components/Privacy/Privacy';
+import  ContactButton  from './Components/ContactSupport/ContactButton';
 function App() {
   return (
+    <>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path="/introduce" element={<Introduce />} />
       <Route path="/register" element={<Register />} />
       <Route path="/privacy" element={<Privacy />} />
+
     </Routes>
+     <ContactButton /></>
   );
 }
 
